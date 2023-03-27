4ba0429acd180bfa410ba1ff50875732f4d29e80
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2023-03-27 22:52:58 +0000

    aa

--- HW2_Draft/part2_partyWiseINPROGRESS/partyWise.js
+++ HW2_Draft/part2_partyWiseINPROGRESS/partyWise.js
@@ -8,11 +8,17 @@
     src = document.getElementById("members");
     target = document.getElementById("dropLists");
     msg = document.getElementById("msg");
+    var democratDropZone = document.getElementById("democrats");
+    var republicanDropZone = document.getElementById("republicans");
 
     // Add drag and drop event listeners
     src.addEventListener('dragstart', dragStartHandler);
     target.addEventListener('dragover', dragOverHandler);
     target.addEventListener('drop', dropHandler);
+    democratDropZone.addEventListener("dragenter", dragEnterDemocratsHandler);
+    democratDropZone.addEventListener("drop", dropDemocratsHandler);
+    republicanDropZone.addEventListener("dragenter", dragEnterRepublicansHandler);
+    republicanDropZone.addEventListener("drop", dropRepublicansHandler);
 
     // Read data from partyList.xml
     var xmlhttp = new XMLHttpRequest();
@@ -33,7 +39,7 @@
         var json2 = json.senators.senator;
         json2.forEach(item=> {
              console.log(item.name["#text"]);
-             src.innerHTML += `<li draggable="true"> ${item.name["#text"]} </li>`
+             src.innerHTML += `<li draggable="true" data-party=${item.party["#text"]} > ${item.name["#text"]} </li>`
         })
         
       }
@@ -45,6 +51,20 @@
 
   function dragStartHandler(e) {
     e.dataTransfer.setData("text", e.target.innerHTML);
+    var party = getParty(e.target.innerHTML);
+    console.log('party data--',  e.target.getAttribute("data-party"))
+    e.dataTransfer.setData("party", e.target.getAttribute("data-party"));
+  }
+  
+  function getParty(item) {
+    var partyList = JSON.parse(localStorage.getItem("partyList"));
+    var senators = partyList.senators.senator;
+    for (var i = 0; i < senators.length; i++) {
+      if (senators[i].name["#text"] === item) {
+        return senators[i].party["#text"];
+      }
+    }
+    return null;
   }
 
   function dragOverHandler(e) {
@@ -54,8 +74,77 @@
   function dropHandler(e) {
     e.preventDefault();
     var data = e.dataTransfer.getData("text");
-    e.target.innerHTML += "<li>" + data + "</li>";
+    var node = document.createElement("LI");
+    //node.innerHTML = data;
+    //e.target.appendChild(node);
   }
+  function dragEnterDemocratsHandler(e) {
+    e.preventDefault();
+    var party = e.dataTransfer.getData("party");
+    console.log('party', party)
+    if (party === "Democrat") {
+      e.target.classList.add("drag-over");
+    }
+  }
+  
+  function dropDemocratsHandler(e) {
+    e.preventDefault();
+
+    var data = e.dataTransfer.getData("text");
+    var party = e.dataTransfer.getData("party");
+    if (party === "Democrat") {
+
+
+      // get sub items
+      var node = document.createElement("LI");
+      node.innerHTML = data;
+      e.target.appendChild(node);
+    }
+  }
+  
+  function dragEnterRepublicansHandler(e) {
+    var partyList = JSON.parse(localStorage.getItem("partyList"));
+    var senators = partyList.senators.senator;
+    var republicanFound = false;
+  
+    senators.forEach(function(senator) {
+      if (senator.party === "Republican") {
+        republicanFound = true;
+        return;
+      }
+    });
+  
+    var party = e.dataTransfer.getData("party");
+    if (republicanFound && party === "Republican") {
+      e.preventDefault();
+      e.target.classList.add("over");
+    }
+  }
+  
+  function dropRepublicansHandler(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    var targetList = e.target.querySelector("ul");
+   
+    var party = e.dataTransfer.getData("party");
+    console.log("gd repu----", {e: e.dataTransfer, eleent: e.target, party, data}, e.dataTransfer.get)
+  
+
+    if (party === "Republican") {
+      var node = document.createElement("LI");
+      node.innerHTML = data;
+      e.target.appendChild(node);
+    }
+
+    /*
+    if (targetList) {
+      targetList.innerHTML += "<li>" + data + "</li>";
+      var sourceList = document.getElementById("members");
+      var item = sourceList.querySelector("li:contains(" + data.trim() + ")");
+      item.parentNode.removeChild(item);
+    } */
+  }
+
 
   function xmlToJson(xml) {
     // Create the return object
