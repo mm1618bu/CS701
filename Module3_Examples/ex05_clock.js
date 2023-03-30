3f11c260ddd4f963839004af42678ad0c5fbf101
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2023-03-30 01:24:29 +0000

    00

---
+++ Module3_Examples/ex05_clock.js
@@ -0,0 +1,32 @@
+function TimeController($scope){
+    $scope.format = 'M/d/yy h:mm:ss a';
+}
+
+angular.module('time',[])
+    .directive('myCurrentTime', function($timeout, dateFilter){
+        return function(scope, element, attrs){
+            var format, timeoutId;
+
+            function updateTime(){
+                element.text(dateFilter(new Date(), format));
+            }
+
+            scope.$watch(attrs.myCurrentTime, function(value){
+                format = value;
+                updateTime();
+            });
+
+            function updateLater(){
+                timeoutId = $timeout(function(){
+                    updateTime();
+                    updateLater();
+                },1000);
+            }
+
+            element.bind('$destroy',function(){
+                $timeout.cancel(timeoutId);
+            });
+
+            updateLater();
+        }
+    });
