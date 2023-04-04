bd3445d4f57e7483737a1a32c70259f988855a9f
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2023-04-04 00:42:50 +0000

    00

--- Module4_Examples/sample01/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/sample01.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
