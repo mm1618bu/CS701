d8cb94754310d4419a01e57ef66017c1b1245fb6
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2023-04-06 01:30:21 +0000

    00

---
+++ Module4_Examples/shopping-cart/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
