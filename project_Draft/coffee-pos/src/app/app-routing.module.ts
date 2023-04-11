bbf8ec61ad4a7942a4ae08fc539914041c8ccea0
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2023-04-11 22:43:31 +0000

    00

--- project_Draft/coffee-pos/src/app/app-routing.module.ts
+++ project_Draft/coffee-pos/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { OrderComponent } from './order/order.component';
